8ce6bb81dcbcd517670d1636375046b7e396b779
Đức Hiệp <duchiep17122002@gmail.com>
2022-04-02 15:45:00 +0700

    First commit

---
+++ resources/js/recent.js
@@ -0,0 +1,32 @@
+let files = [
+    {
+        name: '',
+        content: '',
+        date: ''
+    },
+];
+
+function start() {
+    if (sessionStorage.getItem('documents')) {
+        files = JSON.parse(sessionStorage.getItem('documents'));
+
+        //Add files to list
+        let ul = document.querySelector('#file-list');
+        for (let i = 1; i < files.length; i++) {
+            let li = document.createElement('li');
+            let name = files[i].name;
+            let date = files[i].date;
+            const file = `<img src="resources\\icon\\document.png" class="grid-img">
+                          <span class="grid-title">${name}</span>
+                          <span class="grid-date">Last edited: ${date}</span>`;
+            
+            li.insertAdjacentHTML('beforeend', file);
+            li.addEventListener('click', function() {
+                location.href = `writer.html?fileName=${files[i].name}`;
+            });
+            ul.appendChild(li);
+        }
+    }
+}
+
+start();
