9abe88ae4710e69c7d85d474a665663dd9c2b5dc
Đức Hiệp <duchiep17122002@gmail.com>
2022-04-07 20:58:51 +0700

    Last basic touch

--- resources/js/recent.js
+++ resources/js/recent.js
@@ -1,32 +1,36 @@
 let files = [
-    {
-        name: '',
-        content: '',
-        date: ''
-    },
+  {
+    name: "",
+    content: "",
+    date: "",
+  },
 ];
 
 function start() {
-    if (sessionStorage.getItem('documents')) {
-        files = JSON.parse(sessionStorage.getItem('documents'));
+  if (sessionStorage.getItem("documents")) {
+    files = JSON.parse(sessionStorage.getItem("documents"));
 
-        //Add files to list
-        let ul = document.querySelector('#file-list');
-        for (let i = 1; i < files.length; i++) {
-            let li = document.createElement('li');
-            let name = files[i].name;
-            let date = files[i].date;
-            const file = `<img src="resources\\icon\\document.png" class="grid-img">
+    files.sort(function (a, b) {
+      return new Date(b.date) - new Date(a.date);
+    });
+
+    //Add files to list
+    let ul = document.querySelector("#file-list");
+    for (let i = 1; i < files.length; i++) {
+      let li = document.createElement("li");
+      let name = files[i].name;
+      let date = files[i].date;
+      const file = `<img src="resources\\icon\\document.png" class="grid-img">
                           <span class="grid-title">${name}</span>
                           <span class="grid-date">Last edited: ${date}</span>`;
-            
-            li.insertAdjacentHTML('beforeend', file);
-            li.addEventListener('click', function() {
-                location.href = `writer.html?fileName=${files[i].name}`;
-            });
-            ul.appendChild(li);
-        }
+
+      li.insertAdjacentHTML("beforeend", file);
+      li.addEventListener("click", function () {
+        location.href = `writer.html?fileName=${files[i].name}`;
+      });
+      ul.appendChild(li);
     }
+  }
 }
 
 start();
